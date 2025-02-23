97024b3b1480b1c1bcaf02d0bccdc95f8b49a964
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-02-22 16:33:47 -0800

    Optimizations

--- agent.py
+++ agent.py
@@ -17,14 +17,33 @@ class Agent:
         visit_info = VisitInfo(self.location, self.senses_smell(), self.senses_wind())
         self.search_knowledge.process_visit(visit_info)
 
-    def safe_neighbors(self):
-        safe_map = self.search_knowledge.safe_map()
-        neighbors = self.location.neighbors()
-        return [x for x in neighbors if safe_map.is_marked_at(x)]
+    # def safe_neighbors(self):
+    #     safe_map = self.search_knowledge.safe_map()
+    #     neighbors = self.location.neighbors()
+    #     return [x for x in neighbors if safe_map.is_marked_at(x)]
 
-    def unvisited_safe_neighbors(self):
-        safe_neighbors = self.safe_neighbors()
-        return [x for x in safe_neighbors if not self.search_knowledge.is_room_visited(x)]
+    def is_useless(self):
+        # return True if agent has no unvisited neighbors or all unvisited neighbors are pits
+        unvisited_neighbors = self.unvisited_neighbors()
+        if len(unvisited_neighbors) == 0:
+            print(self.location)
+            return True
+        elif not self.search_knowledge.pits_tracker.all_hazards_found:
+            return False
+        else:
+            return False
+            # for neighbor in unvisited_neighbors:
+            #     if self.search_knowledge.pits_tracker.naive_possible_hazard_map().is_marked_at(neighbor):
+            #         return False
+            # return True
+
+    def unvisited_neighbors(self):
+        neighbors = self.location.neighbors()
+        return [x for x in neighbors if not self.search_knowledge.is_room_visited(x)]
+
+    # def unvisited_safe_neighbors(self):
+    #     safe_neighbors = self.safe_neighbors()
+    #     return [x for x in safe_neighbors if not self.search_knowledge.is_room_visited(x)]
 
     def __repr__(self):
         return f"Agent at {self.location}"
--- multi_agent_solver.py
+++ multi_agent_solver.py
@@ -8,19 +8,29 @@ class MultiAgentSolver:
         self.cave_info = cave_info
         self.search_knowledge = SearchKnowledge(len(cave_info.pits))
         self.agents = [Agent(CaveRoom(0, 0), self.search_knowledge, cave_info)]
-        self.update_agents()
         self.gold_found = False
+        self.safe_map = None
+        self.update()
 
     def update_agents(self):
         for i in range(len(self.agents)):
             self.agents[i].update_knowledge()
 
+    def update_safe_map(self):
+        self.safe_map = self.search_knowledge.safe_map()
+        self.safe_map = self.search_knowledge.safe_map()
+
+    def update(self):
+        self.update_agents()
+        self.update_safe_map()
+
     def explore_multiply(self):
         unvisited_safe_rooms = set()
 
         for agent in self.agents:
-            unvisited_safe_neighbors = agent.unvisited_safe_neighbors()
-            unvisited_safe_rooms = unvisited_safe_rooms.union(unvisited_safe_neighbors)
+            unvisited_neighbors = agent.unvisited_neighbors()
+            safe_unvisited_neighbors = [x for x in unvisited_neighbors if self.safe_map.is_marked_at(x)]
+            unvisited_safe_rooms = unvisited_safe_rooms.union(safe_unvisited_neighbors)
 
         new_agents = []
         for room in unvisited_safe_rooms:
@@ -31,14 +41,17 @@ class MultiAgentSolver:
             new_agents.append(new_agent)
 
         self.agents.extend(new_agents)
-        self.update_agents()
+        self.update()
 
         return len(new_agents) > 0
+
+    def prune_agents(self):
+        self.agents = [agent for agent in self.agents if not agent.is_useless()]
 
     def run(self):
         while self.explore_multiply():
             # self.search_knowledge.print()
             # print()
-            pass
+            self.prune_agents()
 
         return self.gold_found
